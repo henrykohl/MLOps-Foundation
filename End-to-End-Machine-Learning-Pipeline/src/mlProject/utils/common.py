37105d03aa6bde56e81b168156b6a1b7bfa42dd8
Henry Hsu <43148403+henrykohl@users.noreply.github.com>
2026-05-28 19:30:26 +0800

    common file updated

--- End-to-End-Machine-Learning-Pipeline/src/mlProject/utils/common.py
+++ End-to-End-Machine-Learning-Pipeline/src/mlProject/utils/common.py
@@ -1,11 +1,11 @@
 import os
-from box.exceptions import BoxValueError
+from box.exceptions import BoxValueError # 這通常在處理空的或格式錯誤的內容時會觸發。
 import yaml
 from mlProject import logger
-import json
+import json                              # 這讓你能夠處理 JSON 格式的資料轉換（編碼與解碼）。
 import joblib
 from ensure import ensure_annotations
-from box import ConfigBox
+from box import ConfigBox                # 能把 Python 的字典包裝起來，使可以像存取物件屬性一樣讀取設定。
 from pathlib import Path
 from typing import Any
 
@@ -26,14 +26,14 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         ConfigBox: ConfigBox type
     """
     try:
-        with open(path_to_yaml) as yaml_file:
-            content = yaml.safe_load(yaml_file)
+        with open(path_to_yaml) as yaml_file:    # 唯讀模式打開路徑 path_to_yaml 的檔案，並將其指派給 yaml_file。
+            content = yaml.safe_load(yaml_file)  # 使用 PyYAML 庫的 safe_load 函數，將 YAML 檔案內容解析成 Python 的原始資料結構（通常是字典或列表）。
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
-            return ConfigBox(content)
-    except BoxValueError:
-        raise ValueError("yaml file is empty")
-    except Exception as e:
-        raise e
+            return ConfigBox(content)            # 將解析後的資料（content）傳入 ConfigBox 進行封裝並回傳。
+    except BoxValueError:                        # 如果傳給 ConfigBox 的內容有問題（例如檔案內容是空的），則會捕捉到這個特定錯誤。 
+        raise ValueError("yaml file is empty")   # 若捕捉到上述錯誤，就丟出一個自定義的 ValueError，並顯示「yaml file is empty」訊息。
+    except Exception as e:                       # 捕捉所有其他類型的錯誤（例如檔案路徑不存在、權限不足等）。
+        raise e                                  # 將捕捉到的原始錯誤直接再次拋出，不做額外處理。
     
 
 
@@ -59,8 +59,8 @@ def save_json(path: Path, data: dict):
         path (Path): path to json file
         data (dict): data to be saved in json file
     """
-    with open(path, "w") as f:
-        json.dump(data, f, indent=4)
+    with open(path, "w") as f: # 使用上下文管理器打開位於 path 路徑的檔案，將打開的檔案物件命名為 f。
+        json.dump(data, f, indent=4) # 將 data 物件序列化為 JSON 格式，並寫入檔案物件 f 中。設定縮排為 4 個空格。
 
     logger.info(f"json file saved at: {path}")
 
@@ -77,8 +77,11 @@ def load_json(path: Path) -> ConfigBox:
     Returns:
         ConfigBox: data as class attributes instead of dict
     """
-    with open(path) as f:
-        content = json.load(f)
+    
+    
+    # 換後的資料，供後續程式使用。
+    with open(path) as f:       # 開啟位於 path 路徑的檔案--預設為唯讀模式 ("r")--將讀取到的物件命名為 f
+        content = json.load(f)  # 從中讀取 JSON 字串，並將其反序列化回資料結構（如 dict），後指派給變數 content。
 
     logger.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
@@ -92,7 +95,7 @@ def save_bin(data: Any, path: Path):
         data (Any): data to be saved as binary
         path (Path): path to binary file
     """
-    joblib.dump(value=data, filename=path)
+    joblib.dump(value=data, filename=path) # 此方法，專門用於將指定要儲存的物件 data 儲存到 path 所指定儲存的檔案路徑與名稱
     logger.info(f"binary file saved at: {path}")
 
 
@@ -106,7 +109,7 @@ def load_bin(path: Path) -> Any:
     Returns:
         Any: object stored in the file
     """
-    data = joblib.load(path)
+    data = joblib.load(path) # 開啟位於 path 的檔案，並將其二進位內容反序列化成原本的物件（例如機器學習模型），並指派給變數 data。
     logger.info(f"binary file loaded from: {path}")
     return data
 
